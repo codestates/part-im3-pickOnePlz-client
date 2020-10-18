c5e100addbb3ffb3969dc1da84b9dab53c0fd94e
rbdkqk <rbdkqk@gmail.com>
2020-10-18 17:14:29 +0900

    commit to merge
      - Login.js / Signup.js : 내용수정 없음
      - App.js / App.css : 버튼까지 만든 상태

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./Header"
-import Main from "./Main"
-import Login from "./Login"
-import Signup from "./Signup"
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import Header from "./Header";
+import Main from "./Main";
+import Login from "./Login";
+import Signup from "./Signup";
+import NewQuestion from "./NewQuestion";
+
+import "./App.css";
 
 function App() {
   return (
@@ -17,9 +21,17 @@ function App() {
             <div>
               <Route path="/login" component={Login} />
               <Route path="/signup" component={Signup} />
+              <Route path="/newQuestion" component={NewQuestion} />
               <Route exact path="/" component={Main} />
             </div>
           </Switch>
+          <div>
+            <Link to="/newQuestion">
+              <Button variant="primary" className="floatingButton">
+                <span className="buttonInside">+</span>
+              </Button>
+            </Link>
+          </div>
         </Router>
       </div>
     </div>
--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
@@ -12,7 +11,7 @@ function Login() {
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Text className="text-muted">
             We`ll never share your email with anyone else.
-    </Form.Text>
+          </Form.Text>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
@@ -23,11 +22,11 @@ function Login() {
         </Form.Group> 체크박스 부분 사용여부 확인*/}
         <Button variant="primary" type="submit">
           Submit
-  </Button>
-</Form>
-    <Link to="/signup">골라죠 회원이 아니신가요?</Link>
+        </Button>
+      </Form>
+      <Link to="/signup">골라죠 회원이 아니신가요?</Link>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
