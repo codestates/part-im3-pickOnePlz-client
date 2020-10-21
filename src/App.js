5640a0428d5e72a1020848ed9faae564e95d8d8a
rbdkqk <rbdkqk@gmail.com>
2020-10-22 01:39:26 +0900

    Failed : refactoring `Signup` with redux
    아래와 같은 에러가 발생함
      - /signup 접속시,
      - Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object.
      - Check the render method of `Context.Consumer`.
      - 구글링해서 나오는 답들은 나에게는 안맞는듯?
      - 에러 화면에서 `webpack/bootstrap:784` 이런 메세지도 있는데, 이게 문제일까?

--- src/App.js
+++ src/App.js
@@ -3,36 +3,45 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+import "./App.css";
+
 import Header from "./Header";
 import Main from "./Main";
 import Login from "./Login";
-import Signup from "./Signup";
+// import Signup from "./components/Signup";
+import SignupContainer from "./containers";
 import NewQuestion from "./NewQuestion";
 
-import "./App.css";
+const store = createStore(reducers, applyMiddleware(thunk));
 
 function App() {
   return (
     <div className="App">
       <div className="Body">
-        <Router>
-          <Header />
-          <Switch>
+        <Provider store={store}>
+          <Router>
+            <Header />
+            <Switch>
+              <div>
+                <Route path="/login" component={Login} />
+                <Route path="/signup" component={SignupContainer} />
+                <Route path="/newQuestion" component={NewQuestion} />
+                <Route exact path="/" component={Main} />
+              </div>
+            </Switch>
             <div>
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/newQuestion" component={NewQuestion} />
-              <Route exact path="/" component={Main} />
+              <Link to="/newQuestion">
+                <Button variant="primary" className="floatingButton">
+                  <span className="buttonInside">+</span>
+                </Button>
+              </Link>
             </div>
-          </Switch>
-          <div>
-            <Link to="/newQuestion">
-              <Button variant="primary" className="floatingButton">
-                <span className="buttonInside">+</span>
-              </Button>
-            </Link>
-          </div>
-        </Router>
+          </Router>
+        </Provider>
       </div>
     </div>
   );
