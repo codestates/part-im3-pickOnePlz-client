e0eb1ba699b4b4636f590467438ce2a983b5f665
rbdkqk <rbdkqk@gmail.com>
2020-10-24 23:37:27 +0900

    restore all

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import {
+  SignupContainer,
+  LoginContainer,
+  HeaderContainer,
+  MainContainer,
+  MypageContainer,
+  NewQuestionContainer,
+} from './containers';
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Body">
+        <Router>
+          <HeaderContainer />
+          <Switch>
+            <div>
+              <Route path="/mypage" component={MypageContainer} />
+              <Route path="/login" component={LoginContainer} />
+              <Route path="/signup" component={SignupContainer} />
+              <Route path="/newQuestion" component={NewQuestionContainer} />
+              <Route exact path="/" component={MainContainer} />
+            </div>
+          </Switch>
+          <div>
+            <Link to="/newQuestion">
+              <Button variant="primary" className="floatingButton">
+                <span className="buttonInside">+</span>
+              </Button>
+            </Link>
+          </div>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
