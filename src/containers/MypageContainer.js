f1db65e5aac1a79ed707adae05df371b6894fe0a
sueryong <csr0604@hanmail.net>
2020-10-24 23:01:50 +0900

    [react-redux-refactorying] 리액트 리덕스 리팩토링

---
+++ src/containers/MypageContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Mypage from '../components/Mypage';
+
+const MyPageContainer = () => {
+    return <Mypage />
+}
+
+export default MyPageContainer;
---
+++ src/containers/index.js
@@ -0,0 +1,15 @@
+import HeaderContainer from './HeaderContainer';
+import LoginContainer from './LoginContainer';
+import MainContainer from './MainContainer';
+import NewQuestionContainer from './NewQuestionContainer';
+import SignupContainer from './SignupContainer';
+import MypageContainer from './MypageContainer';
+
+export {
+    SignupContainer,
+    LoginContainer,
+    HeaderContainer,
+    MainContainer,
+    MypageContainer,
+    NewQuestionContainer,
+  };
