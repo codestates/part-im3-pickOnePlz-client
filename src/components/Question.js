a160715e1786e34bbc97b73aa8831108baf82876
rbdkqk <rbdkqk@gmail.com>
2020-10-29 01:22:04 +0900

    [파일명 수정] Main.js / MainContainer.js
    각각 Question.js / QuestionContainer.js 로 수정함

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import "./Main.css";
+import "./Question.css";
 
-export default function Main(props) {
-  // Main 이거 이름 별로인 듯.
+export default function Question(props) {
+  // Main 이거 이름 별로인 듯. => Question 으로 수정함
   const handleDeleteReq = () => {
     if (props.currentUser === null) {
       console.log("로그인이 되어있지 않습니다.");
@@ -23,28 +23,32 @@ export default function Main(props) {
     </div>
   );
 
-  let eachQuestion__mainPage = (
+  let eachQuestion = (
     <div className="eachQuestion__mainPage">
       <div className="questionTitle__mainPage">{props.title}</div>
       <div className="allAnswers__mainPage">
         <div className="answer__mainPage">
           <div>{props.answer_1.message}</div>
-          <button onClick={()=>{
-            props.votingQuestion(props.questionId, props.answer_1.id)
-            }}>
+          <button
+            onClick={() => {
+              props.votingQuestion(props.questionId, props.answer_1.id);
+            }}
+          >
             투표
-            </button>
+          </button>
           <div>투표수</div>
           <div>{props.answer_1.votingCount}</div>
         </div>
         <div style={{ display: "inline-block", margin: "5px" }}>vs</div>
         <div className="answer__mainPage">
           <div>{props.answer_2.message}</div>
-          <button onClick={()=>{
-            props.votingQuestion(props.questionId, props.answer_2.id)
-            }}>
+          <button
+            onClick={() => {
+              props.votingQuestion(props.questionId, props.answer_2.id);
+            }}
+          >
             투표
-            </button>
+          </button>
           <div>투표수</div>
           <div>{props.answer_2.votingCount}</div>
         </div>
@@ -57,5 +61,5 @@ export default function Main(props) {
   // console.log(eachQuestion__mainPage._owner.key); // 나중에, 각 question의 key를 잡기 위해서 써먹어야 할 것 같은데...
   // 각 answer에도 key 이런걸로 접근할 수 있어야 할 것인데..어떻게 하지?
 
-  return <div>{eachQuestion__mainPage}</div>;
+  return <div>{eachQuestion}</div>;
 }
